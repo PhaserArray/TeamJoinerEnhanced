9792f75a0775f4b2b80c4a70c0b9a7ffde8967a3
PhaserArray <me@phaserarray.me>
2019-12-06 19:03:26 +0200

    Blank plugin

---
+++ TeamJoinerEnhanced.cs
@@ -0,0 +1,17 @@
+﻿using Rocket.Core.Plugins;
+
+namespace PhaserArray.TeamJoinerEnhanced
+{
+    public class TeamJoinerEnhanced : RocketPlugin<TeamJoinerEnhancedConfiguration>
+    {
+	    protected override void Load()
+	    {
+		    base.Load();
+	    }
+
+	    protected override void Unload()
+	    {
+		    base.Unload();
+	    }
+    }
+}
