bce4f9e9b70e14156467a70d751d3fd1ac61fea9
PhaserArray <me@phaserarray.me>
2019-12-06 23:14:58 +0200

    Untested but finished

--- TeamJoinerEnhanced.cs
+++ TeamJoinerEnhanced.cs
@@ -1,17 +1,96 @@
-﻿using Rocket.Core.Plugins;
+﻿using Rocket.Core;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
 
 namespace PhaserArray.TeamJoinerEnhanced
 {
-    public class TeamJoinerEnhanced : RocketPlugin<TeamJoinerEnhancedConfiguration>
-    {
-	    protected override void Load()
-	    {
-		    base.Load();
-	    }
+	public class TeamJoinerEnhanced : RocketPlugin<TeamJoinerEnhancedConfiguration>
+	{
+		public const string Version = "v1.0";
+		private TeamJoinerEnhancedConfiguration _config;
 
-	    protected override void Unload()
-	    {
-		    base.Unload();
-	    }
-    }
+		protected override void Load()
+		{
+			_config = Configuration.Instance;
+			U.Events.OnPlayerConnected += OnPlayerConnected;
+			Logger.Log($"Loaded TeamJoinerEnhanced {Version} by PhaserArray");
+		}
+
+		protected override void Unload()
+		{
+			U.Events.OnPlayerConnected -= OnPlayerConnected;
+			Logger.Log($"Unloaded TeamJoinerEnhanced {Version} by PhaserArray");
+		}
+
+		private void OnPlayerConnected(UnturnedPlayer player)
+		{
+			// The player is not in any of the Steam groups.
+			if (_config.Teams.All(team => player.SteamGroupID != team.SteamGroupId))
+			{
+				Logger.Log($"{player.DisplayName} joined without being in any relevant Steam groups.");
+				UnturnedChat.Say(player, _config.NoTeamMessage, Color.yellow);
+				StartCoroutine(SendLinkPopup(player));
+				return;
+			}
+
+			var playerTeam =
+				_config.Teams.Find(team => team.SteamGroupId == player.SteamGroupID);
+			var playerPermissionsGroups = R.Permissions.GetGroups(player, true);
+
+			// The player only has the default permissions group, nothing to clear, just give them the appropriate permissions group.
+			if (playerPermissionsGroups.Count <= 1)
+			{
+				R.Permissions.AddPlayerToGroup(playerTeam.PermissionGroupId, player);
+				UnturnedChat.Say(playerTeam.WelcomeMessage, Color.blue);
+				Logger.Log($"{player.DisplayName} joined without permissions and was given {playerTeam.PermissionGroupId}.");
+			}
+			else
+			{
+				// The player has a permissions group from a different team, so remove all non-ignored permissions and give them the appropriate permissions group.
+				if (playerPermissionsGroups.Any(group =>
+					_config.Teams.Where(team => team != playerTeam).Select(team => team.PermissionGroupId).ToList()
+						.Contains(group.Id)))
+				{
+					if (!_config.WipePermissionsGroups)
+					{
+						Logger.Log($"{player.DisplayName} joined with another team's permissions. WipePermissionsGroups is set to false, so they were not removed and new permissions were not given.");
+						return;
+					}
+
+					var removedPermissionsGroups = new List<string>();
+					foreach (var group in playerPermissionsGroups.Where(group => !_config.IgnoredPermissionsGroupIds.Contains(group.Id)))
+					{
+						R.Permissions.RemovePlayerFromGroup(group.Id, player);
+						removedPermissionsGroups.Add(group.Id);
+					}
+					UnturnedChat.Say(string.Format(_config.PermissionsWipedMessage, string.Join(",", removedPermissionsGroups)), Color.red);
+
+					R.Permissions.AddPlayerToGroup(playerTeam.PermissionGroupId, player);
+					UnturnedChat.Say(playerTeam.WelcomeMessage, Color.blue);
+
+					Logger.Log(
+						$"{player.DisplayName} joined with another team's permissions. {string.Join(",", removedPermissionsGroups)} were removed and was given {playerTeam.PermissionGroupId}.");
+				}
+				else
+				{
+					R.Permissions.AddPlayerToGroup(playerTeam.PermissionGroupId, player);
+					UnturnedChat.Say(playerTeam.WelcomeMessage, Color.blue);
+					Logger.Log($"{player.DisplayName} joined with non-team permissions and was given {playerTeam.PermissionGroupId}.");
+				}
+			}
+		}
+
+		private IEnumerator SendLinkPopup(UnturnedPlayer player)
+		{
+			yield return new WaitForSeconds(2f);
+			player.Player.sendBrowserRequest(_config.NoTeamPopupMessage, _config.NoTeamPopupUrl);
+		}
+	}
 }
--- TeamJoinerEnhancedConfiguration.cs
+++ TeamJoinerEnhancedConfiguration.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using Rocket.API;
+﻿using Rocket.API;
 using Steamworks;
+using System.Collections.Generic;
 
 namespace PhaserArray.TeamJoinerEnhanced
 {
@@ -9,15 +9,13 @@ namespace PhaserArray.TeamJoinerEnhanced
 		public bool WipePermissionsGroups;
 
 		public string NoTeamMessage;
-		public string NoTeamMessageColor;
 
 		public string NoTeamPopupMessage;
 		public string NoTeamPopupUrl;
 
 		public string PermissionsWipedMessage;
-		public string PermissionsWipedColor;
 
-		public List<string> IgnoredPermissionsGroups;
+		public List<string> IgnoredPermissionsGroupIds;
 		public List<TeamJoinerEnhancedConfigurationTeam> Teams;
 
 		public void LoadDefaults()
@@ -26,16 +24,14 @@ namespace PhaserArray.TeamJoinerEnhanced
 
 			NoTeamMessage =
 				"You are not currently in a team. Please ensure you have joined one of the Steam groups and set it as your active group in the main menu under survivors and then group.";
-			NoTeamMessageColor = "#ffeb04";
 
 			NoTeamPopupMessage = "You have not joined a team. Click agree to open a steam guide on how to do that!";
 			NoTeamPopupUrl = "https://steamcommunity.com/sharedfiles/filedetails/?id=1928426601";
 
 			PermissionsWipedMessage =
 				"You were previously on a different team. Permissions do not carry over between sides, the following permission group(s) were removed: {0}";
-			PermissionsWipedColor = "#ff0000";
 
-			IgnoredPermissionsGroups = new List<string>
+			IgnoredPermissionsGroupIds = new List<string>
 			{
 				"mod",
 				"moderator",
@@ -48,13 +44,11 @@ namespace PhaserArray.TeamJoinerEnhanced
 				new TeamJoinerEnhancedConfigurationTeam(
 					"allies",
 					"You have joined the allies and have been given the appropriate permissions group.",
-					"#0000ff",
 					(CSteamID) 103582791465718191u
 					),
 				new TeamJoinerEnhancedConfigurationTeam(
 					"axis",
 					"You have joined the axis and have been given the appropriate permissions group.",
-					"#0000ff",
 					(CSteamID) 103582791465717990u
 					)
 			};
@@ -62,17 +56,26 @@ namespace PhaserArray.TeamJoinerEnhanced
 
 		public struct TeamJoinerEnhancedConfigurationTeam
 		{
-			public string PermissionGroup;
+			public string PermissionGroupId;
 			public string WelcomeMessage;
-			public string WelcomeMessageColor;
 			public CSteamID SteamGroupId;
 
-			public TeamJoinerEnhancedConfigurationTeam(string permissionGroup, string welcomeMessage, string welcomeMessageColor, CSteamID steamGroupId)
+			public TeamJoinerEnhancedConfigurationTeam(string permissionGroupId, string welcomeMessage, CSteamID steamGroupId)
 			{
-				PermissionGroup = permissionGroup;
+				PermissionGroupId = permissionGroupId;
 				WelcomeMessage = welcomeMessage;
-				WelcomeMessageColor = welcomeMessageColor;
 				SteamGroupId = steamGroupId;
+			}
+
+			public static bool operator ==(TeamJoinerEnhancedConfigurationTeam a,
+				TeamJoinerEnhancedConfigurationTeam b)
+			{
+				return a.SteamGroupId == b.SteamGroupId;
+			}
+
+			public static bool operator !=(TeamJoinerEnhancedConfigurationTeam a, TeamJoinerEnhancedConfigurationTeam b)
+			{
+				return a.SteamGroupId != b.SteamGroupId;
 			}
 		}
 	}
